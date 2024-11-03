223c56d1b723ac721f324cea24410a09d5d4e262
ndquang5802 <ndquang5802@gmail.com>
2024-11-03 10:27:40 +0700

    refactor Project

--- src/collections/Models/Projects.ts
+++ src/collections/Models/Projects.ts
@@ -5,41 +5,61 @@ const Projects: CollectionConfig = {
   slug: "projects",
   admin: {
     group: GROUP.MODELS,
-    useAsTitle: "projectName",
+    useAsTitle: "projectName"
   },
   fields: [
     {
       name: "person",
       type: "relationship",
       relationTo: "users",
-      required: true,
+      required: true
     },
     {
       name: "projectName",
       type: "text",
-      required: true,
+      required: true
     },
     {
-      name: "description",
-      type: "textarea",
-    },
-    {
-      name: "startDate",
-      type: "date",
-    },
-    {
-      name: "endDate",
-      type: "date",
-    },
-    {
-      name: "technologiesUsed",
-      type: "richText",
+      type: "row",
+      fields: [
+        {
+          name: "startDate",
+          type: "date",
+          admin: {
+            date: {
+              pickerAppearance: "monthOnly",
+              displayFormat: "MMMM yyyy"
+            }
+          },
+          required: true
+        },
+        {
+          name: "endDate",
+          type: "date",
+          admin: {
+            date: {
+              pickerAppearance: "monthOnly",
+              displayFormat: "MMMM yyyy"
+            }
+          }
+        }
+      ]
     },
     {
       name: "link",
-      type: "text",
+      type: "text"
     },
-  ],
+    {
+      name: "description",
+      type: "richText",
+      required: true
+    },
+    {
+      name: "image", // Field for the profile picture
+      type: "upload",
+      relationTo: "media" // Relates to the Media collection
+    }
+  ]
 };
 
 export default Projects;
